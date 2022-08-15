d44138e8a2d11ea0e1995621542bcf698ca74283
Hasnain Syed <hasnainalsyed@gmail.com>
2022-08-15 13:30:36 +0500

    34 completed

--- src/components/News.js
+++ src/components/News.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
-  constructor() {
-    super();
+
+  static defaultProps = {
+    country: 'us',
+    category: 'general',
+    pageSize: 6
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    category: PropTypes.string,
+    pageSize: PropTypes.number
+  }
+
+  captlizeLetter = string => {
+    string.toLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: true,
       page: 1
     }
+    document.title = `${this.captlizeLetter(this.props.category)} - NewsMonkey`;
   }
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=b5a3192bd39941aebb960b27a5f8d5a0&page=1&pageSize=${this.props.pageSize}`;
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b5a3192bd39941aebb960b27a5f8d5a0&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsData = await data.json();
@@ -24,29 +44,22 @@ export class News extends Component {
     });
   }
 
-  handleNextClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=b5a3192bd39941aebb960b27a5f8d5a0&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsData = await data.json();
-    this.setState({
-      articles: parsData.articles,
-      page: this.state.page + 1,
-      loading: false
-    });
+  async componentDidMount() {
+    this.updateNews();
   }
-  
-  handlePrevClick = async () => {
-    // console.log('Previous');
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=b5a3192bd39941aebb960b27a5f8d5a0&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsData = await data.json();
+
+  handleNextClick = async () => {
     this.setState({
-      articles: parsData.articles,
-      page: this.state.page - 1,
-      loading: false
+      page: this.state.page + 1,
     });
+    this.updateNews();
+  }
+
+  handlePrevClick = async () => {
+    this.setState({
+      page: this.state.page - 1,
+    });
+    this.updateNews();
   }
 
   render() {
@@ -55,17 +68,20 @@ export class News extends Component {
         <div className="container-xl">
           <div className="row">
             <div className="col-12 mb-5">
-              <h2 className="text-center">NewsMonkey - Top Headlines</h2>
-              { this.state.loading && <Spinner /> }
+              <h2 className="text-center">NewsMonkey - Top {this.captlizeLetter(this.props.category)} Headlines</h2>
+              {this.state.loading && <Spinner />}
             </div>
             {!this.state.loading && this.state.articles.map(element => {
               return (
                 <NewsItem
-                  key={element ? element.url : ''}
-                  title={element ? element.title.slice(0, 45) : ''}
-                  description={element ? element.description : ''}
-                  imageUrl={element ? element.urlToImage : 'https://picsum.photos/200/300/?blur'}
-                  newsUrl={element ? element.url : ''} />
+                  key={element.url ? element.url : ''}
+                  title={element.title ? element.title.slice(0, 45) : ''}
+                  description={element.description ? element.description : ''}
+                  imageUrl={element.urlToImage ? element.urlToImage : 'https://picsum.photos/200/300/?blur'}
+                  author={element.author ? element.author : 'Unknown'}
+                  date={element.publishedAt ? element.publishedAt : ''}
+                  source={element.source.name ? element.source.name : 'Unknown'}
+                  newsUrl={element.url ? element.url : ''} />
               );
             })}
             <div className="col-12">
@@ -76,15 +92,15 @@ export class News extends Component {
                       disabled={this.state.page <= 1}
                       className="page-link"
                       onClick={this.handlePrevClick}>
-                        &larr; Previous
+                      &larr; Previous
                     </button>
                   </li>
                   <li className="page-item">
                     <button
-                      disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / this.props.pageSize )}
+                      disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / this.props.pageSize)}
                       className="page-link"
                       onClick={this.handleNextClick}>
-                        Next &rarr;
+                      Next &rarr;
                     </button>
                   </li>
                 </ul>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
 
     return (
       <div className='col-md-4 mb-4'>
         <div className="card h-100">
-          <img src={ imageUrl } className="card-img-top" alt={ title } />
+          <img src={imageUrl} className="card-img-top" alt={title} />
           <div className="card-body">
-            <h5 className="card-title">{ title }</h5>
-            <p className="card-text">{ description }</p>
+            <h5 className="card-title">{title}</h5>
+            <span className="position-absolute top-0 end-0 mt-2 me-2 badge rounded-pill bg-success">
+              {source}
+            </span>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By: {author}, At: {new Date(date).toGMTString()}</small></p>
           </div>
           <div className="card-footer">
-            <a href={ newsUrl } target="_blank" rel='noreferrer' className="btn btn-dark btn-sm">Read more</a>
+            <a href={newsUrl} target="_blank" rel='noreferrer' className="btn btn-dark btn-sm">Read more</a>
           </div>
         </div>
       </div>
